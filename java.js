974845ca14af4092b75086f20ecedc9b7990614f
Alan Paccor <alanpaccor@gmail.com>
2024-01-05 02:22:09 -0500

    going to sleep

---
+++ java.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+    wrapWordsWithSpan("highlightText");
+});
+
+function wrapWordsWithSpan(elementId) {
+    const textContainer = document.getElementById(elementId);
+    const words = textContainer.textContent.split(/\s+/);
+
+    let newText = "";
+
+    words.forEach((word, index) => {
+        const span = document.createElement("span");
+        span.textContent = word + " ";
+        
+        if (index % 2 === 0) {
+            span.classList.add("highlight-word");
+        } else {
+            span.classList.add("random-word");
+        }
+
+        newText += span.outerHTML;
+    });
+
+    textContainer.innerHTML = newText;
+
+    const spans = textContainer.querySelectorAll('span');
+
+    spans.forEach((span, index) => {
+        span.addEventListener('mouseenter', () => {
+            span.classList.add('hovered');
+            spans[index - 1]?.classList.add('hovered');
+            spans[index + 1]?.classList.add('hovered');
+        });
+
+        span.addEventListener('mouseleave', () => {
+            spans.forEach(s => {
+                s.classList.remove('hovered');
+            });
+        });
+    });
+}
